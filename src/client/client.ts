6202a57e576b73bf9d6297eddbcd08248173c2d6
minse317 <s2019w30@e-mirim.hs.kr>
2023-05-07 23:39:39 +0900

    Object Panel

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import {GUI} from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -10,7 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-camera.position.z = 2
+camera.position.z = 2   //카메라 기본 설정값
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -33,17 +34,28 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
-  // render()
+  render()
 }
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 20)
+cameraFolder.open()
+
 function animate() {
   requestAnimationFrame(animate)
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
 
   render()
 
