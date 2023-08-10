bce842ddf796db255d7230d8e0b184e73e12e4a9
minse317 <s2019w30@e-mirim.hs.kr>
2023-08-10 22:35:43 +0900

    MeshPhysicalMaterial

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,7 +6,7 @@ import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight(0xffffff, 2)
+const light = new THREE.PointLight(0xffffff, 1000)
 light.position.set(10, 10, 10)
 scene.add(light)
 
@@ -30,10 +30,20 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshStandardMaterial()
+const material = new THREE.MeshPhysicalMaterial({})
+material.reflectivity = 0
+material.transmission = 1.0
+material.roughness = 0.2
+material.metalness = 0
+material.clearcoat = 0.3
+material.clearcoatRoughness = 0.25
+material.color = new THREE.Color(0xffffff)
+material.ior = 1.2
+material.thickness = 10.0
 
 const texture = new THREE.TextureLoader().load('img/grid.png')
 material.map = texture
+
 const pmremGenerator = new THREE.PMREMGenerator(renderer)
 const envTexture = new THREE.CubeTextureLoader().load(
   [
@@ -47,6 +57,7 @@ const envTexture = new THREE.CubeTextureLoader().load(
   () => {
     material.envMap = pmremGenerator.fromCubemap(envTexture).texture
     pmremGenerator.dispose()
+    scene.background = material.envMap
   }
 )
 
@@ -55,11 +66,11 @@ cube.position.x = 5
 scene.add(cube)
 
 const sphere = new THREE.Mesh(sphereGeometry, material)
-sphere.position.x = 3
+sphere.position.x = 0
 scene.add(sphere)
 
 const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
-icosahedron.position.x = 0
+icosahedron.position.x = 3
 scene.add(icosahedron)
 
 const plane = new THREE.Mesh(planeGeometry, material)
@@ -111,21 +122,28 @@ const data = {
   emissive: material.emissive.getHex(),
 }
 
-const meshStandardMaterialFolder = gui.addFolder('THREE.MeshStandardMaterial')
+const meshPhysicalMaterialFolder = gui.addFolder('THREE.MeshPhysicalMaterial')
 
-meshStandardMaterialFolder.addColor(data, 'color').onChange(() => {
+meshPhysicalMaterialFolder.addColor(data, 'color').onChange(() => {
   material.color.setHex(Number(data.color.toString().replace('#', '0x')))
 })
-meshStandardMaterialFolder.addColor(data, 'emissive').onChange(() => {
+meshPhysicalMaterialFolder.addColor(data, 'emissive').onChange(() => {
   material.emissive.setHex(Number(data.emissive.toString().replace('#', '0x')))
 })
-meshStandardMaterialFolder.add(material, 'wireframe')
-meshStandardMaterialFolder
+
+meshPhysicalMaterialFolder.add(material, 'wireframe')
+meshPhysicalMaterialFolder
   .add(material, 'flatShading')
   .onChange(() => updateMaterial())
-meshStandardMaterialFolder.add(material, 'roughness', 0, 1)
-meshStandardMaterialFolder.add(material, 'metalness', 0, 1)
-meshStandardMaterialFolder.open()
+meshPhysicalMaterialFolder.add(material, 'reflectivity', 0, 1)
+meshPhysicalMaterialFolder.add(material, 'roughness', 0, 1)
+meshPhysicalMaterialFolder.add(material, 'metalness', 0, 1)
+meshPhysicalMaterialFolder.add(material, 'clearcoat', 0, 1, 0.01)
+meshPhysicalMaterialFolder.add(material, 'clearcoatRoughness', 0, 1, 0.01)
+meshPhysicalMaterialFolder.add(material, 'transmission', 0, 1, 0.01)
+meshPhysicalMaterialFolder.add(material, 'ior', 1.0, 2.333)
+meshPhysicalMaterialFolder.add(material, 'thickness', 0, 10.0)
+meshPhysicalMaterialFolder.open()
 
 function updateMaterial() {
   material.side = Number(material.side) as THREE.Side
@@ -134,6 +152,9 @@ function updateMaterial() {
 
 function animate() {
   requestAnimationFrame(animate)
+
+  torusKnot.rotation.x += 0.01
+  torusKnot.rotation.y += 0.01
 
   render()
 
